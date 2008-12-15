ded26578b4cf836454b92dc959b62bdc5f937fa2
T.J. VanSlyke <tj@elctech.com>
2008-12-14 17:57:13 -0800

    added example swatch html generator, now generates tints properly

--- lib/sassafras.rb
+++ lib/sassafras.rb
@@ -9,6 +9,8 @@ require 'color'
 gem     'activesupport'
 require 'activesupport'
 
+require 'erb'
+
 module Sassafras
 
   class Theme
@@ -26,25 +28,55 @@ module Sassafras
       @base_rgb.html
     end
 
-    def sass
-      "!base = #{base}"
+    def colors 
+      {}.merge(@tints.colors)
     end
+
+    def sass
+      returning "" do |str|
+        colors.each do |name, hex|
+          str << "!#{name} = #{hex}\n"
+        end
+      end
+    end
+
+    def get_binding; binding; end
 
   end
 
   class Tints
 
-    attr_reader :base
+    attr_reader :colors
 
     def initialize(base)
-      @base_rgb   = Color::RGB.const_get(base.to_s.camelize)
+      @rgb = Color::RGB.const_get(base.to_s.camelize)
+      @colors = {
+        :base   => @rgb.html,
+        :mid    => @rgb.lighten_by(50).html,
+        :light  => @rgb.lighten_by(30).html,
+        :pale   => @rgb.lighten_by(10).html
+      }
     end
 
-    def base
-      @base_rgb.html
-    end
+    def base;  @colors[:base];  end
+    def mid;   @colors[:mid];   end
+    def light; @colors[:light]; end
+    def pale;  @colors[:pale];  end
 
   end
 
+  class HTMLSwatch
+
+    def initialize(theme)
+      @theme = theme
+    end
+
+    def output
+      File.open(File.dirname(__FILE__) + '/sassafras/swatch.html.erb') do |f|
+        erb = ERB.new(f.read)
+        erb.run(@theme.get_binding)
+      end
+    end
+  end
   
 end
--- spec/sassafras_spec.rb
+++ spec/sassafras_spec.rb
@@ -2,31 +2,70 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe Sassafras do
 
-  describe "when creating a basic color scheme" do
- 
-    before :each do
-      @sassafras = Sassafras::Theme.new(:basic, :red)
+  describe Sassafras::Theme do 
+
+    describe "when creating a basic color scheme" do
+   
+      before :each do
+        @sassafras = Sassafras::Theme.new(:basic, :red)
+      end
+
+      it "sets the base color" do
+        @sassafras.base.should == "#ff0000"
+      end
+
+      it "sets the tint colors" do
+        @sassafras.tints.base.should == "#ff0000"
+      end
+
     end
 
-    it "sets the base color" do
-      @sassafras.base.should == "#ff0000"
-    end
+    describe "when generating Sass output" do
+      
+      before :each do
+        @sassafras = Sassafras::Theme.new(:basic, :red)
+      end
+      
+      it "outputs the base color" do
+        @sassafras.sass.should include("!base = #ff0000")
+      end
 
-    it "sets the tint colors" do
-      @sassafras.tints.base.should == "#ff0000"
     end
 
   end
 
-  describe "when generating Sass output" do
-    
-    before :each do
-      @sassafras = Sassafras::Theme.new(:basic, :red)
+  describe Sassafras::Tints do
+
+    before :each do 
+      @tints = Sassafras::Tints.new(:red)
     end
-    
-    it "outputs the base color" do
-      @sassafras.sass.should include("!base = #ff0000")
+
+    describe "accessing tints" do
+
+      it "has a base tint" do
+        @tints.base.should == '#ff0000'
+      end
+
+      it "has a mid tint" do
+        @tints.mid.should == '#ffbfbf'
+      end
+
+      it "has a light tint" do
+        @tints.light.should == '#ff8080'
+      end
+
+      it "has a pale tint" do
+        @tints.pale.should == '#ff4040'
+      end
+
     end
+
+    describe "when generating Sass output" do
+
+      it "outputs five levels of tints" do
+      end
+    end
+
 
   end
 
